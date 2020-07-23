cf1b8dfd3613ca24b3332bc431f1fde73cc59570
root <826032504@qq.com>
2020-07-23 20:22:34 +0800

    update models

--- networks/autoencoders.py
+++ networks/autoencoders.py
@@ -1,86 +1,96 @@
 import torch
 from torch import nn
 
+def conv3x3(in_channels, out_channels):
+    return nn.Conv2d(in_channels, 4*out_channels, kernel_size=3, stride=1, padding=1)
 
-def conv3x3(in_channels, out_channels, stride=1, groups=1, dilation=1):
-    return nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding = dilation, groups=groups, bias=False, dilation=dilation)
+def conv5x5(in_channels, out_channels):
+    return nn.Conv2d(in_channels, out_channels, kernel_size=5, stride=2, padding=1)
 
-def conv1x1(in_channels, out_channels, stride=1):
-    return nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride, bias=False)
+def batch_norm(in_channels):
+    return nn.BatchNorm2d(in_channels)
 
-def Maxpooling(kernel_size=3,stride=2):
-    return nn.MaxPool2d(kernel_size, stride)
-def Upsample(scale=2):
-    return nn.PixelShuffle(scale)
-
-class BasicBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, stride=1, groups=1, norm_layer=None):
-        super(BasicBlock, self).__init__()
-        if norm_layer is None:
-            norm_layer = nn.BatchNorm2d
-        self.conv1 = conv3x3(in_channels, out_channels, stride)
-        self.bn1 = norm_layer(out_channels)
-        self.relu = nn.ReLU(inplace=True)
-        self.conv2 = conv3x3(out_channels, out_channels)
-        self.bn2 = norm_layer(out_channels)
-        self.stride = stride
-
+class Flatten(nn.Module):
+    def __init__(self):
+        super(Flatten, self).__init__()
     def forward(self, x):
-        indentity = x
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.relu(out)
+        return x.view(x.size(0), -1)
 
-        out = self.conv2(out)
-        out = self.bn2(out)
-
-        out+=indentity
-        #out = self.pooling(x)
-        out = self.relu(out)
-
+class Reshape(nn.Module):
+    def __init__(self):
+        super(Reshape, self).__init__()
+    def forward(self, x):
+        return x.view(-1, 1024, 8, 8)
 
 class AutoEncoder(nn.Module):
     def __init__(self):
         super(AutoEncoder, self).__init__()
-        self.conv1 = BasicBlock(64, 64)
-        self.conv2 = BasicBlock(128, 128)
-        self.conv3 = BasicBlock(512 ,512)
-        self.conv4 = conv1x1()
-        self.linear = nn.Linear(16*16*1024, 2048)
-        self.linear2 = nn.Linear(2048, 16*16*1024)
+        self.conv1 = conv5x5(3, 64)
+        self.bn1 = batch_norm(64)
+        self.conv2 = conv5x5(64, 128)
+        self.bn2 = batch_norm(128)
+        self.conv3 = conv5x5(128, 256)
+        self.bn3 = batch_norm(256)
+        self.conv4 = conv5x5(256, 512)
+        self.bn4 = batch_norm(512)
+        self.conv5 = conv5x5(512, 1024)
+        self.bn5 = batch_norm(1024)
+        self.relu = nn.ReLu()
+        self.linear1 = nn.Linear(8*8*1024, 1024)
+        self.linear2 = nn.Linear(1024, 8*8*1024)
+        self.upsample = nn.PixelShuffle(2)
+        self.flatten = Flatten()
+        self.reshape = Reshape()
+        self.deconv1 = conv3x3(1024, 512)
+        self.deconv2 = conv3x3(512, 256)
+        self.deconv3 = conv3x3(256, 128)
+        self.deconv4 = conv3x3(128, 64)
+        self.deconv5 = conv3x3(64 ,3)
+        self.sigmoid = nn.Sigmoid()
 
     
     def forward(self, x):
-        x = conv3x3(3, 64)(x)
-        x = Maxpooling()(x) #64,128
-        x = self.conv1(x) #64,128
-        x = conv1x1(64, 128)(x) #128, 128
-        x = Maxpooling()(x) #128, 64
-        x = self.conv2(x) #128, 64
-        x = conv1x1(128, 512)(x) #512, 64
-        x = Maxpooling()(x) #512, 32
-        x = self.conv3(x) #512, 32
-        x = conv1x1(512, 1024)(x) #1024, 32
-        x = Maxpooling()(x) #1024, 16
-        x = x.view(x.size(0), -1)
-
-        encoded = self.linear(x)
+        x = self.conv1(x) #(64, 128, 128)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.conv2(x) #(128, 64, 64)
+        x = self.bn2(x)
+        x = self.relu(x)
+        x = self.conv3(x) #(256 ,32 ,32)
+        x = self.bn3(x)
+        x = self.relu(x)
+        x = self.conv4(x) #(512 ,16 ,16)
+        x = self.bn4(x)
+        x = self.relu(x)
+        x = self.conv5(x) #(1024 ,8 ,8)
+        x = self.bn5(x)
+        x = self.relu(x)
+        x = self.flatten(x)
+        encoded = self.linear1(x)
         
         x = self.linear2(encoded)
-        x = x.view(-1, 1024, 16, 16)
-        x = Upsample()(x)
-        x = conv1x1(1024, 512)(x) #512, 32
-        x = self.conv3(x) #512, 32
-        x = Upsample()(x) #512, 64
-        x = conv1x1(512, 128)(x) #128, 64
-        x = self.conv2(x) #128, 64
-        x = Upsample()(x) #128, 128
-        x = conv1x1(128, 64)(x) #64, 128
-        x = self.conv1(x) #64, 128
-        x = Upsample()(x) #64, 256
-        decoded = conv3x3(64, 3)(x)
-
-
-
+        x = self.reshape(x)
+        x = self.deconv1(x)
+        x = self.bn4(x)
+        x = self.relu(x)
+        x = self.upsample(x) #(512, 16, 16)
+        x = self.deconv2(x)
+        x = self.bn3(x)
+        x = self.relu(x)
+        x = self.upsample(x) #(256, 32, 32)
+        x = self.deconv3(x)
+        x = self.bn2(x)
+        x = self.relu(x)
+        x = self.upsample(x) #(128, 64, 64)
+        x = self.deconv4(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.upsample(x) #(64, 128, 128)
+        x = self.deconv5(x)
+        x = self.relu(x)
+        x = self.upsample(x) #(3, 256, 256)
+        decoded = self.sigmoid(x)
 
         return encoded, decoded
+
+
