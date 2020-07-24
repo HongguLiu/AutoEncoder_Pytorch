403eafa46da1a609724ad366414173cc97a8235c
HongguLiu <826032504@qq.com>
2020-07-24 09:36:50 +0800

    add lr_scheduler

--- train.py
+++ train.py
@@ -4,9 +4,12 @@ from torchvision import transforms
 from networks.autoencoders import AutoEncoder
 import pdb
 from datasets import MyDataset
+import os
+from torch.optim import lr_scheduler
 
 num_epoches = 100
-batch_size = 128
+batch_size = 256
+learning_rate = 2e-4
 
 transformer = transforms.Compose([
     transforms.Resize((256, 256)),
@@ -29,7 +32,9 @@ model = AutoEncoder()
 model = model.to(device)
 model = torch.nn.DataParallel(model)
 criterion = torch.nn.L1Loss()
-optimizer = torch.optim.Adam(model.parameters(), lr=0.0001)
+#optimizer = torch.optim.Adam(model.parameters(), lr=0.0001)
+optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate, betas=(0.9, 0.999), eps=1e-08, weight_decay=0.1)
+scheduler = lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.5, patience=2, verbose=True)
 best_loss = 1000
 
 for epoch in range(num_epoches):
@@ -62,5 +67,6 @@ for epoch in range(num_epoches):
     if not (epoch+1) % 10:
         model_wts = model.module.state_dict() if isinstance(model, torch.nn.DataParallel) else model.state_dict()
         torch.save(model_wts, os.path.join('./output', str(epoch+1)+'_'+str(val_loss/batch_size)[0:6]+'_'+'ae_real.pth'))
+    scheduler.step(val_loss)
     print('Epoch [%d/%d], Val Loss:%.4f'%(epoch+1, num_epoches, val_loss/len(val_data)))
 torch.save(best_model_wts, os.path.join('./output', str(best_epoch+1)+'_'+str(best_loss/batch_size)[0:6]+'_'+'best_ae.pth'))
